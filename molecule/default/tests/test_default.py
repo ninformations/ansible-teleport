9410c91b612e670bde193658fcffbebcc78ba58e
Gerald Ng <ngjingsiang@gmail.com>
2019-07-23 16:04:49 +0800

    Init teleport galaxy with molecule

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_teleport_installation(host):
+    teleport_run = host.run("teleport version")
+    tctl_run = host.run("tctl version")
+
+    assert 'Teleport Enterprise v3.1.6' in teleport_run.stdout
+    assert 'Teleport Enterprise v3.1.6' in tctl_run.stdout
+
+
+@pytest.mark.parametrize('path', [
+    '/var/lib/teleport'
+])
+def test_teleport_directory(host, path):
+    dir = host.file(path)
+
+    assert dir.is_directory
+    assert dir.user == 'ubuntu'
+    assert dir.group == 'ubuntu'
