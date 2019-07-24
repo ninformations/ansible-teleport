b36a8c136f320cdf8497cd9232d7bd0c8a65ea00
Gerald Ng <ngjingsiang@gmail.com>
2019-07-24 17:56:18 +0800

    Add more tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,10 +14,13 @@ def test_teleport_installation(host):
 
     assert 'Teleport v3.1.6' in teleport_run.stdout
     assert 'Teleport v3.1.6' in tctl_run.stdout
+    assert not teleport_download.exists
 
 
 @pytest.mark.parametrize('path', [
-    '/var/lib/teleport'
+    '/var/lib/teleport',
+    '/etc/teleport/',
+    '/etc/teleport/ssl'
 ])
 def test_teleport_directory(host, path):
     dir = host.file(path)
@@ -25,3 +28,10 @@ def test_teleport_directory(host, path):
     assert dir.is_directory
     assert dir.user == 'ubuntu'
     assert dir.group == 'ubuntu'
+
+
+def test_teleport_running(host):
+    service = host.service('teleport')
+
+    assert service.is_running
+    assert service.is_enabled
